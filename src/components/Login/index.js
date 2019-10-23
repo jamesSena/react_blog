25585f7d4696c0829b1136484a1193b6e07b3fc2
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-23 16:37:30 -0300

    adicionando o CSS para o login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import firebase from '../../firebase';
-
+import './login.css'
 class Login extends Component{
     constructor(props){
         super(props);
@@ -9,12 +9,37 @@ class Login extends Component{
             email:'',
             password:'',
         };
+        this.entrar = this.entrar.bind(this);
+        this.login = this.login.bind(this);
+
     }
+    entrar(e){
+        e.preventDefault();
+
+    }
+
+    login = async() => {
+        const {email, password} = this.state;
+        try{
+            await firebase.login(email, password).catch((error)=>{
+                if(error.code ==='auth/user-not-found'){
+                    alert("usuario invalido");
+                }else{
+                    alert('Codigo de erro: ' + error.code);
+                }
+
+            });
+        }catch(e){
+            alert('error: ' + e);
+
+        }
+    }
+
     render(){
         return(
             <div>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={this.entrar} id="login">
                     <label>Email: </label> <br/>
                     <input type="email" autoComplete="off" autoFocus value={this.state.email}
                     onChange={(event)=> this.setState({email: event.target.value})} placeholder="teste@teste.com"
@@ -24,6 +49,8 @@ class Login extends Component{
                     <input type="password" autoComplete="off" value={this.state.password}
                     onChange={(event)=> this.setState({password: event.target.value})} placeholder="*******"
                     /><br/>
+                    <button type="submit">Entrar</button>
+                    <Link to="/register"> Ainda não possui uma conta? </Link>
                 </form>
             </div>
         );
